a1d4132dc6f1f4ad49de9cd8760abd4e7cc88886
waibinin <99268684+waibinin@users.noreply.github.com>
2024-07-28 16:11:49 -0300

    change clippy errors

--- src/ui/palette.rs
+++ src/ui/palette.rs
@@ -3,8 +3,7 @@ use bevy::prelude::*;
 pub const BUTTON_HOVERED_BACKGROUND: Color = Color::srgb(0.171, 0.148, 0.122);
 pub const BUTTON_PRESSED_BACKGROUND: Color = Color::srgb(0.151, 0.171, 0.178);
 
-pub const BUTTON_TEXT: Color =
-    Color::srgb(0.9764705882352941, 0.7607843137254902, 0.16862745098039217);
+pub const BUTTON_TEXT: Color = Color::srgb(0.976470, 0.7607843, 0.16862);
 pub const LABEL_TEXT: Color = Color::srgb(0.105, 0.79, 0.98);
 pub const HEADER_TEXT: Color = Color::srgb(0.867, 0.827, 0.412);
 
